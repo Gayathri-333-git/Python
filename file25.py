4310757c3be8e43641ecdb13b575438618b40232
Gayathri-333-git <gayathrim9078@gmail.com>
2026-07-21 20:59:41 +0530

    Add files via upload

---
+++ file25.py
@@ -0,0 +1,7 @@
+def leap_year(n):
+    if(n % 4 == 0 and n % 100 != 0) or (n % 400 == 0):
+        print("Leap year")
+    else:
+        print("Not leap year")
+n = int(input("Enter a number: "))
+leap_year(n)   
